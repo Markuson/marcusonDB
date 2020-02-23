6c76f5af7daeac61d75c02bc51954208361391e0
Markuson <el.marcuson@gmail.com>
2020-02-23 19:15:30 +0100

    starting the app

---
+++ marcusondb-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from './Components/Login'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Login />
+      </header>
+    </div>
+  );
+}
+
+export default App;
